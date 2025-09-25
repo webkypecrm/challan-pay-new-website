396f4b812577fb33973317cd97649527802f4ef8
webkype <webkypecrm@gmail.com>
2025-09-25 19:41:49 +0530

    25/09/2025

--- src/app/payment-summary/page.tsx
+++ src/app/payment-summary/page.tsx
@@ -4,22 +4,14 @@ import React from "react";
 import PaymentSummaryHeader from "../components/payment-summary/PaymentSummaryHeader";
 import Header from "../components/common/Header";
 import { PaymentSummaryTabs } from "../components/payment-summary/PaymentSummaryTabs";
-import BottomSheet from "../components/common/BottomSheet";
-import { useRouter } from "next/navigation";
+//import BottomSheet from "../components/common/BottomSheet";
 
 function page() {
-  const router = useRouter();
   return (
     <>
       <Header />
       <PaymentSummaryHeader />
       <PaymentSummaryTabs />
-      <BottomSheet
-        amount="₹ 4000"
-        subtitle="Total Challan Amount"
-        buttonText="Proceed to pay"
-        onButtonClick={() => router.push("/payment-success")}
-      />
     </>
   );
 }
