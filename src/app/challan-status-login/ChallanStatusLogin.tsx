6448d4a3b81ba11aec4dd30acb2972e1b39dd7fa
webkype <webkypecrm@gmail.com>
2025-10-15 13:22:51 +0530

    15/10---

--- src/app/challan-status-login/ChallanStatusLogin.tsx
+++ src/app/challan-status-login/ChallanStatusLogin.tsx
@@ -173,11 +173,22 @@ export default function ChallanStatusLogin() {
       if (response?.data) {
         const result = response.data;
         setUser(result);
-        if (result?.subscriber?.id && result?.vehicle?.id) {
-          localStorage.setItem("subscriberId", result.subscriber.id);
-          localStorage.setItem("vehicleId", result.vehicle.id);
-        } else {
-          console.warn("Missing subscriber or vehicle data:", result);
+        // if (result?.subscriber?.id && result?.vehicle?.id) {
+        //   localStorage.setItem("subscriberId", result.subscriber.id);
+        //   localStorage.setItem("vehicleId", result.vehicle.id);
+        // } else {
+        //   console.warn("Missing subscriber or vehicle data:", result);
+        // }
+        if (typeof window !== "undefined") {
+          if (result?.subscriber?.id) {
+            localStorage.setItem(
+              "subscriberId",
+              result.subscriber.id.toString()
+            );
+          }
+          if (result?.vehicle?.id) {
+            localStorage.setItem("vehicleId", result.vehicle.id.toString());
+          }
         }
       } else {
         console.error("No data found in response:", response);
