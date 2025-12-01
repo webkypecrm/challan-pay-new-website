3f25e8bf02a0df27e124c06d3ff682a051d25db3
webkype <webkypecrm@gmail.com>
2025-12-01 23:36:56 +0530

    final

--- src/app/ClientWrapper.tsx
+++ src/app/ClientWrapper.tsx
@@ -32,14 +32,14 @@ export default function ClientWrapper({
               },
             }}
           />
-          {/* <noscript>
+          <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-5HQP9D3"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
             ></iframe>
-          </noscript> */}
+          </noscript>
           <ConditionalHeader />
           {children}
         </ChallanProvider>
